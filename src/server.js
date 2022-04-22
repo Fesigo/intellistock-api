57e4ba556edfc3a3cb078bb796fd2a1cbe7f1bed
felipe.siqueira <felipesiqgodoy@gmail.com>
2022-04-22 17:07:41 -0300

    Add mysql database and sequelize setup

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const db = require('./models');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,6 +9,11 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+app.listen(port, async () => {
+    try {
+        await db.sequelize.authenticate();
+        console.log(`Server started on port ${port}`);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+    }
 })
